5898c3404617246abde3d6e45b53aa545f00b853
ExuDenis <lolup24@mail.ru>
2024-09-19 17:27:43 +0300

    add Constants

---
+++ API/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Денис Филатов on 19.09.2024.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "cpXs45VJuuRy_L2iKGZo7OXBndP3JxTCBuD41OMTkJs"
+    static let secretKey = "dje45kKjfQo5nZn63I0vTaKfx7U4E9IJys4INtsGc30"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com/")!
+}
+
+
