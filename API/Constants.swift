90682043abc83c3d53acd73c0bbc4ea5182907ad
ExuDenis <lolup24@mail.ru>
2024-09-21 17:49:11 +0300

    add WebViewViewController

--- API/Constants.swift
+++ API/Constants.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-enum Constants {
-    static let accessKey = "cpXs45VJuuRy_L2iKGZo7OXBndP3JxTCBuD41OMTkJs"
-    static let secretKey = "dje45kKjfQo5nZn63I0vTaKfx7U4E9IJys4INtsGc30"
-    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
-    static let accessScope = "public+read_user+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com/")!
-}
+
+     let AccessKey = "cpXs45VJuuRy_L2iKGZo7OXBndP3JxTCBuD41OMTkJs"
+     let SecretKey = "dje45kKjfQo5nZn63I0vTaKfx7U4E9IJys4INtsGc30"
+     let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+     let AccessScope = "public+read_user+write_likes"
+     let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
+
 
 
